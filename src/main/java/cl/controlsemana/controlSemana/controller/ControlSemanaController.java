52d01fde18f090417ce76a8df437e8676feb95de
universeflow <67252374+universeflow@users.noreply.github.com>
2025-08-17 17:14:40 -0400

    Update ControlSemanaController.java

--- src/main/java/cl/controlsemana/controlSemana/controller/ControlSemanaController.java
+++ src/main/java/cl/controlsemana/controlSemana/controller/ControlSemanaController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-@CrossOrigin(origins = {"http://localhost:3000", "semana-con-mi-hija.vercel.app"})
 public class ControlSemanaController {
 
     @Autowired
