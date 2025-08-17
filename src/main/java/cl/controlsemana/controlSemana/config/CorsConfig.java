f1fff30940d7d1a0685d615205aacc8ef825dfcf
universeflow <67252374+universeflow@users.noreply.github.com>
2025-08-17 14:55:28 -0400

    Update CorsConfig.java

--- src/main/java/cl/controlsemana/controlSemana/config/CorsConfig.java
+++ src/main/java/cl/controlsemana/controlSemana/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(frontendUrl)); // Usar la propiedad parametrizada
+        configuration.setAllowedOrigins(List.of("http://localhost:3000",frontendUrl)); // Usar la propiedad parametrizada
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
