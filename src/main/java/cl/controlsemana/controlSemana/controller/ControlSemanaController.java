886c714fbe3b0968f2a2274ba401ccf7631682ee
universeflow <67252374+universeflow@users.noreply.github.com>
2025-08-19 10:14:02 -0400

    Update ControlSemanaController.java

--- src/main/java/cl/controlsemana/controlSemana/controller/ControlSemanaController.java
+++ src/main/java/cl/controlsemana/controlSemana/controller/ControlSemanaController.java
@@ -5,13 +5,11 @@ import cl.controlsemana.controlSemana.service.FechaService;
 import cl.controlsemana.controlSemana.controller.dto.FechaDto;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.OPTIONS})
 public class ControlSemanaController {
 
     @Autowired
