4b5d65b59358134e0b8cf1c955f02ad82a49896a
universeflow <67252374+universeflow@users.noreply.github.com>
2025-08-19 10:55:54 -0400

    Delete src directory

--- src/main/java/cl/controlsemana/controlSemana/service/FechaService.java
+++
@@ -1,8 +0,0 @@
-package cl.controlsemana.controlSemana.service;
-
-import cl.controlsemana.controlSemana.controller.dto.FechaDto;
-
-public interface FechaService {
-
-    FechaDto validarFecha(String fechaRequest);
-}
